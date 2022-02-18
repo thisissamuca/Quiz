6175e95cfb6490ab360fafce3f18d7c9f35f011d
samuca <augustto.sam3@gmail.com>
2022-02-18 01:00:47 -0300

    Estrutura básica criada
    - Várias modificações em pastas e arquivos para melhor organização.
    
    - Aprimoramento do código para minimizar espaço e facilitar a organização.
    
    - Estilo do QUIZ foi predefinida.
    
    - Lógica do QUIZ em desenvolvimento.

---
+++ js/questions.js
@@ -0,0 +1,111 @@
+
+/* ------------------------------------------------------------------------------- */
+
+/* -------------------- ** PERGUNTAS ** ----------------------- */
+
+var perguntas = [
+
+    // 1
+
+    { pergunta: "Qual característica abaixo não pertence ao relevo planície?",
+
+      respostas: ['Áreas majoritariamente planas', 'Geralmente são de baixa altitude', 'Regiões que não são propensas a receberem cursos de água', 'Recebem grande quantidade de sedimentos de outras regiões'],
+
+      acerto: "resp2"
+
+    },
+
+    // 2
+
+    { pergunta: "áreas vegetais NÃO são capazes de influenciar... :",
+
+      respostas: ['A contenção de raios ultravioletas emitidas pelo sol', 'A elevação da umidade relativa do ar', 'Aumento do índice pluviométrico', 'A condutibilidade elétrica de nuvens e a frequência de trovões'],
+
+      acerto: "resp3"
+
+    },
+
+    // 3
+    
+    { pergunta: "Quais os pares de características que uma montanha geralmente apresenta?",
+
+      respostas: ['Alta altitude e vegetação coberta', 'Paisagem acidentada e tendência à vulcanismo', 'Cume bem definido e proximidade com   corpos de água', 'Paisagem glacial e alta altitude'],
+
+      acerto: "resp1"
+
+    },
+
+    // 4
+
+    { pergunta: "Os seres decompositores apresentam um papel muito importante na natureza. Qual das função são VERDADEIRAS?",
+
+    respostas: ['Se alimentar de carnes ou vegetais', 'Devolver a matéria orgânica para o ciclo energético', 'Os seres decompositores não possuem uma função definida', 'Produzir energia para a cadeia trófica'],
+
+    acerto: "resp1"
+
+    },
+
+    // 5
+
+    { pergunta: "Qual dos elementos químicos FAZEM parte do grupo das conhecidas terras raras?",
+
+    respostas: ['Li, Mg, Ag e Zn', 'Ca, K, Na e Mn', 'Ni, Au, Pt e Hg', 'La, Ce, Nd, e Sm'],
+
+    acerto: "resp3"
+
+    },
+
+    // 6
+
+    { pergunta: "Existem algumas possibilidades dentro do príncipio de Gause, qual alternativa NÃO apresenta uma das possibilidades?",
+
+    respostas: ['Uma das espécies sofrerá esclavagismo', 'Uma das espécies será extinta', 'Uma das espécies irá migrar', 'Uma ou todas as espécies irão alterar seus nichos ecológicos para evitar competição'],
+
+    acerto: "resp0"
+
+    },
+
+    // 7
+
+    { pergunta: "Quais das características geológicas NÃO fazem parte do Brasil?",
+
+    respostas: ['Possui grande parte do território composto por bacias sedimentares e escudos cristalinos', 'Apresenta a segunda maior reserva de terras raras do mundo', 'Há poucos dobramentos modernos devido à posição geográfica', 'Altitudes baixas e médias predominam a região'],
+
+    acerto: "resp2"
+
+    },
+
+    // 8
+
+    { pergunta: "O período pré-cambriano ocorreu os eventos mais importantes da terra. Qual foi o único evento NÃO presente nesse período?",
+
+    respostas: ['Ínicio do movimento tectônico', 'Modificações nas condições climáticas e oceanográficas que permitiu a origem da vida', 'Formação da crosta terrestre e grande atividade vulcânica', 'Aparecimento dos primeiros vegetais e animais'],
+
+
+    acerto: "resp2"
+
+    },
+
+    // 9
+
+    { pergunta: "Os relevos possuem grande diversidade e características que definem uma região geográfica. Qual dos grupos abaixo NÃO faz referência a um relevo?",
+
+    respostas: ['Depressão, Cuesta e Chapada', 'Morro, Colina e montanha', 'Floresta, deserto e campo', 'Falésia, planalto e planície'],
+
+    acerto: "resp2"
+
+    },
+
+    // 10
+
+    { pergunta: "Os seres produtores são aqueles que produzem energia trófica primária para a cadeia alimentar. Indique qual animal abaixo FAZ parte dos seres produtores:",
+
+    respostas: ['Vaca', 'Fitoplâncton', 'Abelha', 'Fungo'],
+
+    acerto: ""
+
+    },
+
+]
+
+/* ------------------------------------------------------------------------------- */
